804d6d8f0c8b5d0f8541104967d06683f143f4cf
Wassim-RF <wassimrifi0313@gmail.com>
2026-01-19 17:29:38 +0100

    Creation du front-end du login

--- app/views/layout/head.php
+++ app/views/layout/head.php
@@ -3,6 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/assets/style/output.css">
+    <link rel="stylesheet" href="/public/assets/style/output.css">
     <title>Minerva</title>
-</head
+</head>
