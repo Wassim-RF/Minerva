261d15dd270e7125e4a68c83fd647c254908045b
bouqrt <bouchraelqrt@gmail.com>
2026-01-19 23:44:12 +0100

    la creation de la classe student extended from user

---
+++ app/models/entities/student.php
@@ -0,0 +1,16 @@
+<?php
+namespace App/Models/Entities
+require_once "User.php";
+
+class Student extends User {
+    private int $class_id;
+
+    public function __construct(int $id = 0, string $name = "", string $email = "", string $password = "", int $class_id = 0) {
+        parent::__construct($id, $name, $email, $password, 'student');
+        $this->class_id = $class_id;
+    }
+
+    public function getClassId(): int { return $this->class_id; }
+    public function setClassId(int $class_id): void { $this->class_id = $class_id; }
+}
+?>
