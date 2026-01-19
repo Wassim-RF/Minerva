b9e54423c0f08546aa2ab401ed993428a9ca02ff
bouqrt <bouchraelqrt@gmail.com>
2026-01-19 17:33:41 +0100

    added the methods in the user class

--- app/models/entities/user.php
+++ app/models/entities/user.php
@@ -1,9 +1,61 @@
 <?php 
-
+namespace App/Models/Entities
 class User{
     protected int $id;
     protected string $name;
     protected string $email;
     protected string $password;
     protected string $role; 
+
+
+    public function __construct(int $id = 0, string $name = "", string $email = "", string $password = "", string $role = "") {
+        $this->id = $id;
+        $this->name = $name;
+        $this->email = $email;
+        $this->password = $password;
+        $this->role = $role;
+    }
+
+    public function getId(): int {
+        return $this->id;
+    }
+
+    public function getName(): string {
+        return $this->name;
+    }
+
+    public function getEmail(): string {
+        return $this->email;
+    }
+
+    public function getPassword(): string {
+        return $this->password;
+    }
+
+    public function getRole(): string {
+        return $this->role;
+    }
+
+    public function setId(int $id): void {
+        $this->id = $id;
+    }
+
+    public function setName(string $name): void {
+        $this->name = $name;
+    }
+
+    public function setEmail(string $email): void {
+        $this->email = $email;
+    }
+
+    public function setPassword(string $password): void {
+        $this->password = $password;
+    }
+
+    public function setRole(string $role): void {
+        $this->role = $role;
+    }
 }
+?>
+   
+
