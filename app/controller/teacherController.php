a65779e41d2d2f6f6560c2b8617dc6268e134285
Wassim-RF <wassimrifi0313@gmail.com>
2026-01-22 07:22:08 +0100

    feat: Ajoute d'envoiye un email ou il y a le mot de passe du student

--- app/controller/teacherController.php
+++ app/controller/teacherController.php
@@ -3,9 +3,12 @@
 
     require_once __DIR__ . '/../models/repositories/teacherRepositories.php';
     require_once __DIR__ . '/../models/services/teacherServices.php';
+    require_once __DIR__ . '/../models/services/emailServices.php';
 
     use App\Models\Repositories\TeacherRepositories;
     use App\Models\Services\TeacherServices;
+    use App\Models\Services\EmailServices;
+
     class TeacherController {
         private TeacherServices $teacherServices;
 
@@ -19,9 +22,12 @@
                 $name  = $_POST['name'];
                 $email = $_POST['email'];
                 $password = substr(md5(rand()), 0, 8);
-                $password = password_hash($password , PASSWORD_DEFAULT);
+                $hashPassword = password_hash($password , PASSWORD_DEFAULT);
 
-                $this->teacherServices->createNewStudentAndToClass($name , $email , $password , $_SESSION['class']['id']);
+                $emailServices = new EmailServices();
+                $emailServices->send($email , $password);
+
+                $this->teacherServices->createNewStudentAndToClass($name , $email , $hashPassword , $_SESSION['class']['id']);
                 header("Location: /teacher/classes/show?id={$_SESSION['class']['id']}");
                 exit();
             }
